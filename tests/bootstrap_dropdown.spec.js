3550a313c8976c0ce001f9766f3e9e31de2fb8be
bnunnagoppula <bnunnagoppula@rvohealth.com>
2025-05-14 11:25:28 +0530

    resolving issues

--- tests/bootstrap_dropdown.spec.js
+++ tests/bootstrap_dropdown.spec.js
@@ -1,24 +1,23 @@
-const {test, expect} = require("@playwright/test");
+const { test, expect } = require("@playwright/test");
 
-test("Dropdown", async ({page}) => {
-
+test("Dropdown", async ({ page }) => {
     await page.goto("https://www.jquery-az.com/boots/demo.php?ex=63.0_2");
 
     await page.locator("button.multiselect").click();
 
-    // check number of options
-    const options = await page.locator("ul li input");
-    await expect(options).toHaveCount(11);
+    // Check number of options
+    const options = page.locator("ul.multiselect-container.dropdown-menu li");
+    await expect(options).toHaveCount(14);
 
-    // select options
-    const optionsList = await page.$$("ul li input");
-    for (const option of optionsList) {
-        const text = await option.textContent().trim();
-        if (text.includes("Angular") || text.includes("Java") || text.includes("Python")) {
-            await option.click();
+    // Loop through options and select Angular, Java, Python
+    const count = await options.count();
+    for (let i = 0; i < count; i++) {
+        const option = options.nth(i);
+        const text = (await option.textContent())?.trim();
+        if (text.includes("Angular") || text.includes("Java") || text.includes("Python") || text.includes("HTML")) {
+            await option.locator("input").click();
         }
-    };
+    }
 
-    await page.waitForTimeout(4000);
-
+    await page.waitForTimeout(4000);  // Just for demo pause
 });
