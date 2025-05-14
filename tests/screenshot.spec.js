78a5313f08664f61a157e8d0e2d67f982498f0e7
bnunnagoppula <bnunnagoppula@rvohealth.com>
2025-05-14 16:26:23 +0530

    record video for tests

--- tests/screenshot.spec.js
+++ tests/screenshot.spec.js
@@ -13,7 +13,7 @@ test("Full page screenshot", async ({page}) => {
     await page.screenshot({path: 'tests/screenshots/amazon_fullpage_' + Date.now() + '.png', fullPage: true});
 });
 
-test.only("Element screenshot", async ({page}) => {
+test("Element screenshot", async ({page}) => {
     await page.goto("https://www.amazon.in/");
 
     await page.locator("#nav-logo").screenshot({path: 'tests/screenshots/amazon_logo_' + Date.now() + '.png'});
